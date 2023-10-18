cf8d428298a7e1c5f38fc5fd029ef77037533a57
orisborlov <mrwiggle40000@gmail.com>
2023-10-18 11:45:44 +0700

    kinda working)

---
+++ Readme.md
@@ -0,0 +1,3 @@
+**This script is designed to display all IP addresses of the protected subnet with the exception of the CE address**
+The script will automatically determine the format of the entered subnet,
+whether it is a CE+ prefix or just a protected subnet (in this case you will be prompted to enter CE separately)
--- script.py
+++ script.py
@@ -1,15 +1,22 @@
 import ipaddress
 
-def exclude_address(network_str, exclude):
+
+def input_data():
+    network_str = input("Введите подсеть в формате x.x.x.x/prefix (например '90.154.2.226/30'): ")
+    
     network = ipaddress.ip_network(network_str, strict=False)
     addresses = list(network.subnets(new_prefix=32))
 
-    if exclude:
+    if(network_str == str(network)):
+        exclude = input("\nВведите адрес CE, если CE и адрес подсети представлены отьельно \n(Если CE и префикс защищаемой подсети предствалены вместе нажмите Enter): ")
+
+        
         exclude_address = ipaddress.ip_network(exclude)
         try:
             addresses.remove(exclude_address)
         except ValueError:
             print(f"Подсеть {exclude} не входит в подсеть {network_str}.")
+
     else:
         exclude_address = ipaddress.ip_network(network_str[:-3] + '/32')
         try:
@@ -18,13 +25,9 @@ def exclude_address(network_str, exclude):
             print(f"Подсеть {exclude} не входит в подсеть {network_str}.")
 
     
-    return addresses[1:-1]
 
-def input_data():
-    network_str = input("Введите подсеть в формате x.x.x.x/prefix (например '90.154.2.226/30'): ")
-    exclude = input("\nВведите адрес CE, если CE и адрес подсети представлены отьельно \n(Если CE и префикс защищаемой подсети предствалены вместе нажмите Enter): ")
     result = ''
-    for subnet in exclude_address(network_str, exclude):
+    for subnet in addresses[1:-1]:
         result += str(subnet) + ', '
 
     print('\n'+result[:-2])
